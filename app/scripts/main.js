2bd9dff93a57aa353d2bc095bdc56ec95cca678e
JoCharlet <jonathan.charlet@he-arc.ch>
2016-06-23 16:21:01 +0200

    8961dd2 Improved project et removed useless CSS files

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function(){
+  'use strict';
+});
+
+
+
+/*
+	$(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			var dist =$(page).offset().top - 40; // correction perso pour éviter un décalage
+			$('html, body').animate( { scrollTop:dist}, speed ); // Go
+			return false;
+		});
+	});
+
+    */
